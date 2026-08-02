d8c8ffd7177f0123a1ac4cb00409da8640f6cc9c
HAFSA-SHAHID-28 <hafsa.shahid.282007@gmail.com>
2026-08-02 13:40:52 -0700

    service and utils folder

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,16 +1,44 @@
-
+import { useContext, createContext, useEffect, useState } from "react";
+import { getToken } from "../utils/auth";
+import { fetchUserFromServer } from "../services/auth";
+import { setUser as User } from "../utils/auth";
 
 const AuthContext = createContext();
 
 //// provider
 
+export const AuthProvider = ({ Children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
+  const loadUser = async () => {
+    const token = getToken();
+    if (!token) {
+      return setLoading(false);
+    }
+    try {
+      const user = await fetchUserFromServer();
+      setUser(user);
+      User(user);
+    } catch (error) {
+      console.log("error is fetching user ", error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
+  useEffect(() => {
+    loadUser();
+  }, []);
 
-() => {
   return (
-   
-  )
-}
+    <AuthContext.Provider value={{ user, setUser, loading }}>
+      {Children}
+    </AuthContext.Provider>
+  );
+};
 
-export default AuthContext
+///// consume
+export const useAuthContext = () => useContext(AuthContext);
+
+
