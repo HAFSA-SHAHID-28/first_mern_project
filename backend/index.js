38c40974bfe886690328567b15f9e79c9b3bde9d
HAFSA-SHAHID-28 <hafsa.shahid.282007@gmail.com>
2026-08-29 11:46:42 +0500

    mern complete

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,30 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from 'express'; 
+import 'dotenv/config';
 import dbConnection from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
-import cors from 'cors';
-
-
+import cors from "cors";
+import blogRouter from './routes/blogRoutes.js';
+import adminRouter from './routes/adminRoutes.js';
+import stripeRouter from './routes/stripeRoutes.js';
+  
 const app = express();
-dotenv.config();
+
 dbConnection();
 
-///// middleware
+////// midddleware
 app.use(express.json());
 app.use(cors());
 
 
-////////  routes
+///// routes
 
 app.use('/api/auth', authRoutes)
+app.use('/api/blog', blogRouter)
+app.use('/api/admin', adminRouter)
+app.use('/api', stripeRouter )
 
+app.get('',(req,res)=>{
+res.send("Backend Deployed")
+})
 
-app.listen(process.env.PORT, () => console.log(`Server is running at port ${process.env.PORT}`));
+app.listen(process.env.PORT, ()=>console.log(`Server is running on port ${process.env.PORT}`));
